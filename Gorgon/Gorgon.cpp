a76fa98bb3aa943d30c108bbee51218134175587
HyPNOTIQ <qline0@gmail.com>
2025-07-03 01:57:57 +0300

    pch

--- Gorgon/Gorgon.cpp
+++ Gorgon/Gorgon.cpp
@@ -1,17 +1,4 @@
-﻿#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-#include <fmt/ostream.h>
-#include <iostream>
-#include <thread>
-#include <filesystem>
-#include <atomic>
-#include <ranges>
-#include <gsl/gsl>
-
-#define VULKAN_HPP_NO_CONSTRUCTORS
-#define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
-#include <vulkan/vulkan_raii.hpp>
-#include <vulkan/utility/vk_small_containers.hpp>
+﻿#include "pch.h"
 
 VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
 
